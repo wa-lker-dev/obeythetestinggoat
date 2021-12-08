6bbac41bc08d34d64637a526984dcafe8567f3c7
jds-walker <jdsw86@gmail.com>
2021-12-09 07:02:26 +1100

    added persona descritpion

--- tests/functional/personas.py
+++ tests/functional/personas.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
-from pytest import fixture
+import pytest
 
-@fixture
+@pytest.fixture
 def browser():
     browser = webdriver.Firefox()
     browser.get('http://localhost:8000')
@@ -11,5 +11,30 @@ def browser():
     browser.close()
 
 
-def test_browser(browser: webdriver.Firefox):
-    assert 'Django' in browser.title
+def test_edith_persona(browser: webdriver.Firefox):
+    # She notices the page title and header mention to-do lists
+    assert 'To-Do' in browser.title, "Browser title was " + browser.title
+
+    # She is invited to enter a to-do item straight away
+
+    # She types "Buy peacock feathers" into a text box (Edith's hobby
+    # is tying fly-fishing lures)
+
+    # When she hits enter, the page updates, and now the page lists
+    # "1: Buy peacock feathers" as an item in a to-do list
+
+    # There is still a text box inviting her to add another item. She
+    # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+    # The page updates again, and now shows both items on her list
+
+    # Edith wonders whether the site will remember her list. Then she sees
+    # that the site has generated a unique URL for her -- there is some
+    # explanatory text to that effect.
+
+    # She visits that URL - her to-do list is still there.
+
+    # Satisfied, she goes back to sleep
+
+if __name__ == '__main__':  
+    pytest.main()
